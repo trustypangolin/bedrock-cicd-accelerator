eb2807a5683273f5435e65eaf7c5ba8feb7632c3
benjamin luton <benjamin@luton.net.au>
2022-03-12 13:05:17 +1000

    git

--- tf/deploy/aws_assumerole_oidc.sh
+++ tf/deploy/aws_assumerole_oidc.sh
@@ -6,7 +6,7 @@ if [[ $(git remote -v | awk '/origin/ && /gitlab/') ]] 2>/dev/null; then
   session_name="GitLabRunner-${CI_PROJECT_ID}-${CI_PIPELINE_ID}"
   role_name=GitLab-Bootstrap
 else
-  echo "found something else, not gitlab"
+  echo "found something else, not GitLab"
 fi
 
 if [[ $(git remote -v | awk '/origin/ && /bitbucket/') ]] 2>/dev/null; then
@@ -17,6 +17,22 @@ if [[ $(git remote -v | awk '/origin/ && /bitbucket/') ]] 2>/dev/null; then
 else
   echo "found something else, not Bitbucket"
 fi
+
+if [[ $(git remote -v | awk '/origin/ && /github/') ]] 2>/dev/null; then
+  echo "found GitHub remote fetch"
+  # echo "token = $jwt_token" | base64 --decode
+  # OIDC_TOKEN=$(curl -sLS "${ACTIONS_ID_TOKEN_REQUEST_URL}&audience=o6s" -H "User-Agent: actions/oidc-client" -H "Authorization: Bearer $ACTIONS_ID_TOKEN_REQUEST_TOKEN")
+  # echo "token val = " $OIDC_TOKEN | jq -r '.value' | rev
+  echo "This should be running under the GitHub action for most cloud providers"
+  exit 0
+else
+  echo "found something else, not Github"
+fi
+
+if [ ! -f terraform.tfvars ]; then echo "$BEDROCK_TF_VARS" | base64 --decode > "terraform.tfvars"; else echo "tfvars part of repo"; fi
+
+export AWS_REGION=$(cat terraform.tfvars | grep base_region | cut -d '=' -f2 | tr -d '[:blank:]\"')
+export AWS_DEFAULT_REGION=$(cat terraform.tfvars | grep base_region | cut -d '=' -f2 | tr -d '[:blank:]\"')
 
 # Assume the Role that trusts the OIDC IdP using Web Identity
 STS=($(aws sts assume-role-with-web-identity                          \
@@ -37,13 +53,12 @@ STSADMIN=($(aws sts assume-role                                     \
   --duration-seconds 3600                                           \
   --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]'  \
   --output text))
-if [ ! -f terraform.tfvars ]; then echo "$BEDROCK_TF_VARS" | base64 --decode > "terraform.tfvars"; else echo "tfvars part of repo"; fi
 
 # Send these credentials to the .env file as an artifact for subsequent jobs
-echo "AWS_DEFAULT_REGION=$(cat terraform.tfvars | grep base_region | cut -d = -f2 | tr -d '[:blank:]\"')" > credentials.env
+echo "AWS_DEFAULT_REGION=$(cat terraform.tfvars | grep base_region | cut -d '=' -f2 | tr -d '[:blank:]\"')" > credentials.env
 echo "AWS_ACCESS_KEY_ID=${STSADMIN[0]}" >> credentials.env
 echo "AWS_SECRET_ACCESS_KEY=${STSADMIN[1]}" >> credentials.env
 echo "AWS_SESSION_TOKEN=${STSADMIN[2]}" >> credentials.env
 
-# Bitbucket sharing of credentials through a public repo securely
+# Bitbucket/GitHub/GitLab sharing of credentials through the repo securely
 openssl enc -aes-256-cbc -pbkdf2 -base64 -k $ENCKEY -in credentials.env -out securecreds.enc
